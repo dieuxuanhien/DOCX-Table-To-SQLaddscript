4d311d96a741386409e2190be694fbb21eb66223
dieuxuanhien <79910980+dieuxuanhien@users.noreply.github.com>
2024-09-29 21:20:16 +0700

    copy to clipboard

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from docx import Document
+import pyperclip
 
 docx_path = "tables.docx"
 
 doc = Document(docx_path)
-
+# with open("script.txt", 'w') as file:
+#         # This step creates the file or clears it if it already exists
+#         pass  # No action needed, just opening in 'w' mode clears the file
 scriptFile = open("script.txt",'w') 
 
 
@@ -24,8 +27,11 @@ for table in doc.tables:
         values = [ f"'{cell.text.strip() }'"  for cell in tuple.cells]
         sqladd =f"INSERT INTO {tableName} ({",".join(headers)}) VALUES ({",".join(values)});"
         scriptFile.write(sqladd + "\n")
-    
 
+
+with open("script.txt", "r") as s:
+    contents = s.read()
+    pyperclip.copy(contents)
 
 
 scriptFile.close()
