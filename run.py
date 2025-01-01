fb52a47dc2f855b2c3ab76b238365399fa84b6db
Xuan-Hien Dieu <79910980+dieuxuanhien@users.noreply.github.com>
2025-01-01 22:40:15 +0700

    update

--- run.py
+++ run.py
@@ -4,34 +4,29 @@ import pyperclip
 docx_path = "tables.docx"
 
 doc = Document(docx_path)
-# with open("script.txt", 'w') as file:
-#         # This step creates the file or clears it if it already exists
-#         pass  # No action needed, just opening in 'w' mode clears the file
-scriptFile = open("script.txt",'w') 
-
+with open("script.txt", 'w') as file:
+        # This step creates the file or clears it if it already exists
+        pass  # No action needed, just opening in 'w' mode clears the file
+scriptFile = open("script.txt",'w', encoding='utf-8') 
 
 for table in doc.tables:
-
-    
-
     #get table name
-    tableName = table.rows[0].cells[0].text.strip()
-
+    tableName = table.rows[0].cells[0].text.split(" ")[0]
 
     #get headers
-    headers = [cell.text.strip() for cell in table.rows[1].cells]
-
+    headers = [cell.text.strip() for cell in table.rows[0].cells]
+    headers[0] = headers[0].split(" ")[1]
     scriptFile.write(f"-----------------------{tableName} DATA------------------------\n\n")
     #write tuples add
-    for tuple in table.rows[2:]:
-        values = [ f"'{cell.text.strip() }'"  for cell in tuple.cells]
-        sqladd =f"INSERT INTO {tableName} ({",".join(headers)}) VALUES ({",".join(values)});"
+    for tuple in table.rows[1:]:
+        values = []
+        for cell in tuple.cells:
+            cell_text = cell.text.strip()
+            if cell_text == '0'  or ( cell_text.isnumeric() and not cell_text.startswith('0')):
+                values.append(cell_text)
+            else:
+                values.append(f"'{cell_text}'")
+        sqladd = f"INSERT INTO {tableName} ({','.join(headers)}) VALUES ({','.join(values)});"
         scriptFile.write(sqladd + "\n")
-
-
-with open("script.txt", "r") as s:
-    contents = s.read()
-    pyperclip.copy(contents)
-
 
 scriptFile.close()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,32 @@
-docx_path = 'tables.docx'  # Replace with your DOCX file path
+from docx import Document
+import pyperclip
+
+docx_path = "tables.docx"
+
+doc = Document(docx_path)
+with open("script.txt", 'w') as file:
+        # This step creates the file or clears it if it already exists
+        pass  # No action needed, just opening in 'w' mode clears the file
+scriptFile = open("script.txt",'w', encoding='utf-8') 
+
+for table in doc.tables:
+    #get table name
+    tableName = table.rows[0].cells[0].text.split(" ")[0]
+
+    #get headers
+    headers = [cell.text.strip() for cell in table.rows[0].cells]
+    headers[0] = headers[0].split(" ")[1]
+    scriptFile.write(f"-----------------------{tableName} DATA------------------------\n\n")
+    #write tuples add
+    for tuple in table.rows[1:]:
+        values = []
+        for cell in tuple.cells:
+            cell_text = cell.text.strip()
+            if cell_text == '0'  or ( cell_text.isdigit() and not cell_text.startswith('0')):
+                values.append(cell_text)
+            else:
+                values.append(f"'{cell_text}'")
+        sqladd = f"INSERT INTO {tableName} ({','.join(headers)}) VALUES ({','.join(values)});"
+        scriptFile.write(sqladd + "\n")
+
+scriptFile.close()
